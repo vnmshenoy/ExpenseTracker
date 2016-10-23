a0996edd14fee3542b2eb2d5a8effdc132e0ec68
manju <vnmshenoy@gmail.com>
2016-10-23 14:44:18 +1100

    item history

--- www/js/itemHistoryController.js
+++ www/js/itemHistoryController.js
@@ -6,11 +6,17 @@ $stateParams) {
           disableBack: true
       });
       $ionicPlatform.ready(function() {
-        var query = "SELECT  * FROM tblCategoryItems"+
-        " where upper(category_item_name) like '%ONION%'";
+       $scope.itemDetails = [];
+        var query = "SELECT  SUM(category_item_price) as cost, strftime('%m', category_item_date) as month"+
+        " FROM tblCategoryItems"+
+        " where upper(category_item_name) like '%ONION%'"+
+        " GROUP BY strftime('%m', category_item_date)";
+
           $cordovaSQLite.execute(db, query).then(function(res) {
               if(res.rows.length > 0) {
-                  console.log(res);
+                $scope.itemDetails.push({id: res.rows.item(i).id, cost: res.rows[0].cost,
+                  month: res.rows[0].month});
+
               }
           }, function (err) {
               console.error(err);
