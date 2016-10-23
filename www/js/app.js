f22cddb2de8694d2f0f06487ec756b1543f5e083
manju <vnmshenoy@gmail.com>
2016-10-23 22:20:47 +1100

    chart initial draft

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var expenseTracker = angular.module('starter', ['ionic', 'ngCordova']);
+var expenseTracker = angular.module('starter', ['ionic', 'ngCordova','chart.js']);
 var db=null;
 expenseTracker.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
--- www/js/itemHistoryController.js
+++ www/js/itemHistoryController.js
@@ -7,9 +7,16 @@ $stateParams) {
       });
       $ionicPlatform.ready(function() {
        $scope.itemDetails = [];
+       $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
         var query = "SELECT  SUM(category_item_price) as cost, strftime('%m', category_item_date) as month"+
         " FROM tblCategoryItems"+
-        " where upper(category_item_name) like '%ONION%'"+
+        " where upper(category_item_name) like '%APPLE%'"+
         " GROUP BY strftime('%m', category_item_date)";
 
           $cordovaSQLite.execute(db, query).then(function(res) {
