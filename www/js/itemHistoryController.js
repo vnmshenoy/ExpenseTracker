89b46758cf8b606c476704a682d75b6b54806043
manju <vnmshenoy@gmail.com>
2016-10-30 10:54:20 +1100

    categorise by mnth year

--- www/js/itemHistoryController.js
+++ www/js/itemHistoryController.js
@@ -7,17 +7,17 @@ $stateParams) {
       });
       $ionicPlatform.ready(function() {
        $scope.itemDetails = [];
-       $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
-
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-        var query = "SELECT  SUM(category_item_price) as cost, strftime('%m', category_item_date) as month"+
-        " FROM tblCategoryItems"+
+  //      $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // $scope.series = ['Series A', 'Series B'];
+  //
+  // $scope.data = [
+  //   [65, 59, 80, 81, 56, 55, 40],
+  //   [28, 48, 40, 19, 86, 27, 90]
+  // ];
+        var query = "SELECT  SUM(category_item_price) as cost, strftime('%m', category_item_date) as month,"+
+        " strftime('%Y', category_item_date) as year FROM tblCategoryItems"+
         " where upper(category_item_name) like '%APPLE%'"+
-        " GROUP BY strftime('%m', category_item_date)";
+        " GROUP BY month,year";
 
           $cordovaSQLite.execute(db, query).then(function(res) {
             var resultRows=[];
@@ -27,7 +27,7 @@ $stateParams) {
               if(resultRows.length > 0) {
                 for(var i=0;i<resultRows.length;i++){
                   $scope.itemDetails.push({id: resultRows.item(i).id, name:$stateParams.itemName ,cost: resultRows[i].cost,
-                    month: resultRows[i].month});
+                    month: resultRows.item(i).month,year:resultRows.item(i).year});
 
                 }
               }
