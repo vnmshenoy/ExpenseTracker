c5f0ebee04e0e7485455231c2c1fc91e62470a72
manjunath shenoy <manjunath shenoy>
2016-11-07 21:52:48 +1100

    data to create top category spent

--- www/js/topCategoriesController.js
+++ www/js/topCategoriesController.js
@@ -4,11 +4,14 @@ expenseTracker.controller("TopCategoriesController", function($scope,
     $scope.topCategories = [];
     $ionicPlatform.ready(function() {
       var query = "SELECT ca.category_id,ca.category_name,ci.category_item_id,"+
-      " SUM(ci.category_item_price) from tblCategories ca INNER JOIN tblCategoryItems ci"+
+      " SUM(ci.category_item_price) as cost from tblCategories ca INNER JOIN tblCategoryItems ci"+
  " WHERE ca.id=ci.category_id GROUP BY ca.id";
         $cordovaSQLite.execute(db, query, []).then(function(res) {
             if(res.rows.length > 0) {
               console.log(res);
+                    for(var i = 0; i < res.rows.length; i++) {
+              $scope.topCategories.push({id: res.rows.item(i).id, category_id:res.rows.item(i).category_id,category_name: res.rows.item(i).category_name,totalcost: res.rows.item(i).cost});
+}
             }
         }, function (err) {
             console.error(err);
