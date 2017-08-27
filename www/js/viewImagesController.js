dda546e04f75aa3c95f93867edfc3988c251791b
manjunath.shenoy <vnmshenoy@gmail.com>
2017-08-27 16:46:45 +1000

    handled imgs deletion

--- www/js/viewImagesController.js
+++ www/js/viewImagesController.js
@@ -1,9 +1,11 @@
 expenseTracker.controller("ViewImagesController", function($scope,
-    $ionicPlatform, $cordovaSQLite, $stateParams,$window,$location) {
+    $ionicPlatform, $cordovaSQLite, $stateParams,$window,$location,$ionicPopup) {
       $ionicPlatform.ready(function() {
         var name = '';
         $scope.im =[];
+        var  localStorageId = "img"+$stateParams.id;
         var imgs = $window.localStorage.getItem("img"+$stateParams.id);
+        $scope.localStorageId =localStorageId;
         if((imgs != undefined) && (imgs != null) )
           {
             var a = imgs.split('~,');
@@ -18,5 +20,29 @@ expenseTracker.controller("ViewImagesController", function($scope,
        var name = i.substr(i.lastIndexOf('/') + 1);
        var trueOrigin = cordova.file.dataDirectory + name;
        return trueOrigin;
+    }
+    
+    $scope.deleteImg = function(i,im){   
+        var current = i;
+        var indxToRmv=-1;
+        var imArr=im;
+           $ionicPopup.confirm({
+                title: 'Are you sure you want to delete?'
+            })
+            .then(function(result) {
+               if(result !== undefined) {            
+                   for(var j=0;j<imArr.length;j++){
+                          if(imArr[j] === current){
+                            indxToRmv=j;
+                          }
+                   }
+                   if(indxToRmv >=0){//if any img selected to delete
+                        imArr.splice(indxToRmv,1);
+                        window.localStorage.setItem($scope.localStorageId,imArr);//updated local storage with imArr(delted current //img)
+                   }
+                } else {
+                   //  alert("in else");
+                }
+           });   
     }
   });
