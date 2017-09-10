dd90c1ce797b83cd1965229079cdfbd50081065b
manjunath.shenoy <vnmshenoy@gmail.com>
2017-09-10 22:46:23 +1000

    fixed aboutus styling issies

--- www/js/aboutUsController.js
+++ www/js/aboutUsController.js
@@ -2,9 +2,9 @@ expenseTracker.controller("AboutUsController", function ($scope,
   $ionicPlatform) {
 
   $ionicPlatform.ready(function () {
-  
+     console.log("in aboutus");
     }, function (err) {
       console.error(err);
     });
   });
-});
+
