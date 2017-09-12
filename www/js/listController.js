1193cc5708d379ea2dd803805e5768002087e9e6
manjunath.shenoy <vnmshenoy@gmail.com>
2017-09-12 22:39:43 +1000

    added spent amount in list page

--- www/js/listController.js
+++ www/js/listController.js
@@ -4,6 +4,8 @@ expenseTracker.controller("ListController",
     $ionicPlatform.ready(function () {
       $ionicLoading.hide();
       $scope.count = 1;
+      var spent=0;
+      $scope.spent=0;
       $scope.noRecords=true;//flag is used when no records/lists are there for category. if new or 0 records then hasRecords =false;
       var localStorageVal = parseInt($window.localStorage.getItem("count"));
       if (db == null) {
@@ -13,6 +15,7 @@ expenseTracker.controller("ListController",
         $scope.lists = [];
         var i = 0;
         var resRows;
+       
         $scope.count++;
         i = 10 * $scope.count;
         if (localStorageVal > i)
@@ -26,6 +29,7 @@ expenseTracker.controller("ListController",
           if (res.rows.length > 0) {
             for (var i = 0; i < res.rows.length; i++) {
               resRows=res.rows.item(i);
+             
               $scope.lists.push({
                 id: resRows.id, category_id: resRows.category_id, category_item_id: resRows.category_item_id, category_item_name: resRows.category_item_name,
                 category_item_price: resRows.category_item_price, category_item_unit: resRows.category_item_unit, category_item_date: resRows.category_item_date
@@ -49,7 +53,9 @@ expenseTracker.controller("ListController",
         if (res.rows.length > 0) {
           for (var i = 0; i < res.rows.length; i++) {
               resRowsLoad=res.rows.item(i);
-            $scope.lists.push({
+              spent+=parseInt(resRowsLoad.category_item_price);
+               $scope.spent =spent;
+             $scope.lists.push({
               id: resRowsLoad.id, category_id:resRowsLoad.category_id, category_item_id:resRowsLoad.category_item_id, category_item_name: resRowsLoad.category_item_name,
               category_item_price: resRowsLoad.category_item_price, category_item_unit: resRowsLoad.category_item_unit, category_item_date: resRowsLoad.category_item_date
             });
@@ -116,8 +122,10 @@ expenseTracker.controller("ListController",
                 var l = res.rows['item'];
                 cat_item_id = ++l(0).category_item_id;
               }
+               
               var query = "INSERT INTO tblCategoryItems (category_id, category_item_id,category_item_name,category_item_price,category_item_unit,category_item_date) VALUES (?,?,?,?,?,?)";
               $cordovaSQLite.execute(db, query, [$stateParams.categoryId, cat_item_id, dd.CategoryItemName, dd.CategoryItemPrice, dd.CategoryItemUnit, date]).then(function (res) {
+                  $scope.spent+=parseInt(dd.CategoryItemPrice, 10);
                 $scope.lists.push({ id: res.insertId, category_id: $stateParams.categoryId, category_item_id: cat_item_id, category_item_name: $scope.data.CategoryItemName, category_item_price: parseInt(dd.CategoryItemPrice, 10), category_item_unit: dd.CategoryItemUnit, category_item_date: date });
                   $scope.noRecords=false;
               }, function (err) {
