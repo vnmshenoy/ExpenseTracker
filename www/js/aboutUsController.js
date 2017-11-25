4e156dd9b9d9688d9dda72706dad2305ebbd352b
manjunath.shenoy <vnmshenoy@gmail.com>
2017-11-25 21:58:18 +1100

    files

--- www/js/aboutUsController.js
+++ www/js/aboutUsController.js
@@ -1,8 +1,6 @@
 expenseTracker.controller("AboutUsController", function ($scope,
   $ionicPlatform) {
-
   $ionicPlatform.ready(function () {
-     console.log("in aboutus");
     }, function (err) {
       console.error(err);
     });
